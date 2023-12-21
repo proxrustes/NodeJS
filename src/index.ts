b73e45a60d75c643c449d276dc3f78e8350506a9
proxrustes <proxrustes@gmail.com>
2023-12-21 16:48:50 +0100

    + index page

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-const i = 0 + 3
-console.log(i)
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});

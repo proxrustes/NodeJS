0109d85e61a9eaae436479a18d383fe12422d752
proxrustes <proxrustes@gmail.com>
2023-12-21 16:50:00 +0100

    deployment prep

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})

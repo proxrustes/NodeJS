8ab13a02e3b992c62065955ff97d4509bd2655fd
proxrustes <proxrustes@gmail.com>
2023-12-27 22:40:56 +0100

    + dist

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const home_1 = __importDefault(require("./routes/home"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
-});
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
-});
+app.use("/", home_1.default);
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });

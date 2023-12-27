d1361a327b2a7792bd30daeaed1ceab6c70bc208
proxrustes <proxrustes@gmail.com>
2023-12-27 22:36:54 +0100

    + home route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import express, { Request, Response } from 'express'
+import home from "./routes/home"
 
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
-})
-
-app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
-})
-
+app.use("/", home)
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`)
 })
